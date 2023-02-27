16e6e929d49a4298b038aac38ca413e040a68542
Stebbi <stefodd@yahoo.com>
2023-02-27 16:40:24 -0600

    added ability to add pizzas to database after connection restored

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -16,7 +16,7 @@ request.onsuccess = function(event) {
 
     //check if app is online, if yes run uploadPizza() function to send all local db data to api
     if (navigator.online) {
-        // uploadPizza()
+        uploadPizza()
     }
 };
 
@@ -35,3 +35,49 @@ function saveRecord(record) {
     // add record to store with add method
     pizzaObjectStore.add(record);
 }
+
+function uploadPizza() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access object store
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // get all records from store and set to a variable
+    const getAll = pizzaObjectStore.getAll();
+
+    // upon a successful .getAll execution, run this function
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, send to api server
+        if (getAll.result.length > 0) {
+            fetch('/api/pizzas', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                // open one more transaction
+                const transaction = db.transaction(['new_pizza'], 'readwrite');
+                // access the new_pizza object store
+                const pizzaObjectStore = transaction.objectStore('new_pizza');
+                // clear all items in your store
+                pizzaObjectStore.clear();
+
+                alert('All saved pizza have been submitted!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+};
+
+// listen for app coming back online
+window.addEventListener('online', uploadPizza);
